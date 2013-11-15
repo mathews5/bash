f23d8d17f7956f9a8a317fd4282f8a99ffde55b2
Ming ZHAO <ming.zhao@hotmail.fr>
2013-11-15 14:03:42 +0100

    debugging the script

--- script.sh
+++ script.sh
@@ -1,24 +1,29 @@
-#!/bin/bash
+#!/bin/bash  
 
-# print the current terminal name
-tput longname
+# bash -x script.sh/ sh -x script.sh : run script and print each source line with current status
+#set -x: display arguments and commands upon their execution
+#set +x: disable debugging
+#set -v: display input when they are read
+#set +v: disabler  
 
-# move cursos to a position 100,100
-tput cup 100 100
+# the following code will only print value of I
+for i in {1..6}
+do
+set -x
+echo $i
+set +x
+done
+echo "Script executed"
 
-# set background color for terminal
-# tput setb 3 #NO from 0 to 7
-
-# set foreground color
-# tput setf 5
-
-# make the text bold 
-tput bold
-
-# start and end underlining
-tput smul
-tput rmul
-
-
+#we can run the following code with debugging set to "on"
+#_DEBUG=on ./script.sh
+function DEBUG()
+{
+[ "$_DEBUG" == "on" ] && $@ || :
+}
+for i in {1..10}
+do
+DEBUG echo $i
+done
 
 
