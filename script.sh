385cd515584a5ba5adf78653959b5ccbd5095895
Ming ZHAO <ming.zhao@hotmail.fr>
2013-11-15 12:57:47 +0100

    file descriptor creation and usage

--- script.sh
+++ script.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
+# < is an operator used to read from the file stdin
+# > is an operator used to write to a file with truncation
+# >> is an operator used to write a file with append
 
-#file descriptors are integers that are associated with file input and output. the best-known file descriptors are stdin(0), stdout(1) and stderr(2).
+echo this is a test line > input.txt
+#create a file descriptor for reading a file
+exec 3<input.txt
+cat <&3
+#if a second read is needed, another file descriptor must be created before usage
 
-#redirect or save output text to a file
-echo "This is a sample text 1" > temp.txt
-#the contents will be cleared before writing into it
-echo " This is another sample text" >> temp.txt
-#this command will append the new text at the end of file
+exec 4>output.txt
+exec 5>>error.txt
 
-#ls + 2>out.txt #store message into file out.txt
-
-#ls + 2>stderr.txt 1>stdout.txt #store message into two files
-#ls + 2>&1 out.txt #store error message to output file
-
-# TEE make the copy of former command result to the next command
-#cat out.txt | tee kn.txt | cat -n
-
-# tee -a put the new content at the end of file
-#cat out.txt | tee -a kn.txt | cat -n
-
-#between line 23 and line 27(EOF) will appear as stdin data.
-cat <<EOF >log.txt
-LOG FILE HEADER
-This is a test log file
-Function:System statistics
-EOF
