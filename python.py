ca155a964448af033756f858399f6671560f8e9f
Ming ZHAO <ming.zhao@hotmail.fr>
2013-11-17 00:47:39 +0100

    basic knowledge

---
+++ python.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+print "Hello,python!";
+
+# naming convention
+# Class names start with an uppercase letter and all other with a lowercase letter
+# Starting an identifier with a single leading undersocre indicates by convention that the identifier is meant to be private
+# Starting an identifier with two leading underscores indicates a strongly private identifier
+# If the identifier also ends with two trailing underscores, the identifier is a language-defiend special name
+
+# Python use identation to indicate blocks of code 
+
+# Multi-line statements
+# total = a + \
+# 	  b + \
+# 	  c
+
+# statements contained within the {}, [] or () do not need use the line continuation character
+# days = [ 'Monday','Tuesday',
+#		'Wednesday',
+#		'Thursday', 'Friday'] 
+
+# word = 'word'
+# word = "word"
+# word = """ Triple quotes can be used
+#		to span the string
+#		across multiple lines """
+
+
+# if expression:
+#    instruments
+# elif expression:
+#    instruments
+# else:
+#    instruments
+
+
+
