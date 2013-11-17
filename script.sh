e300721f6228ce2e3cf80ce2f659841a7403046a
Ming ZHAO <ming.zhao@hotmail.fr>
2013-11-17 18:41:04 +0100

    command SED

--- script.sh
+++ script.sh
@@ -1,55 +1,28 @@
 #!/bin/bash  
 
-# grep "match_pattern" filename ... ...
-# grep  match_pattern  filename filename1 filename 2 ...
-# it gives the line containing match_pattern
+#replace of by de, only for the first occurrence, put in the console, donot change the file 
+#sed 's/is/est/' sed.txt
 
-# read from stdin
-echo -e "this is a word\nnew line" 
-echo -e "this is a word\nnew line" | grep word
+# replace the first occurence and save to a new file, replace and save to new file
+#sed 's/is/est/' sed.txt > new_sed.txt
 
-# highlignt the word in the line --color
-grep "inside" grep.txt --color=auto
+# replace the first occurence and save to the same file
+#sed -i 's/is/est/' sed.txt
 
-# print only the matching portion
-cat grep.txt | grep -o "inside"  
+# replace all occurrences of pattern 'g'
+#sed 's/is/est/g' sed.txt
 
-# print all of the lines except the line containing match_pattern
-cat grep.txt | grep -v "inside"
+# replace from the N-th occurrence
+#echo 1this2this3this | sed 's/this/THIS/2g'
 
-# count number of lines in which a matching string or regex match appears in a file
-cat grep.txt | grep -c "inside"
+# / is a delimiter, we can use any character, :, |
 
-# count the number of matching items in a file
-cat grep.txt | grep  "inside" | wc -l
+# remove blank line, this command will remove /pattern/d
+#sed '/^$/d' blank.txt  
 
-# print the line number of the match string
-cat grep.txt | grep -n "inside"  
+# matched string notation (&), \w\+ matches every word
+#echo this is an example | sed 's/ \w\+ /[&]/g'
 
-# print the character offset at which a pattern matches, it counts from 0
-echo gun is not unix | grep -b -o "not"
-
-# search over many files and find out in which of the files a certain text matches
-#grep -l "match_pattern" file1 file2 file3 ...
-#file1
-#file2
-
-#recursively search for a text over many directories of descendants
-grep "main(" . -R -n
-
-# ignore case sensitive
-echo hello world | grep -i -o "HELLO"
-
-# matching multiple patterns
-echo this is a line of text | grep -e "this" -e "line" -o 
-
-# matching multiple patterns with a pattern file, add -e to accepte \n
-echo -e "this is a line of text, very cool\nnew cool line" | grep -f pat_file
-
-# search only .c and .cpp files
-# grep "main()" . -r --include *.{c,cpp}
-
-# exclude files/directory
-# grep "main()" . -r --exclude "README"
-# grep "main" . -r --exclude-dir "Documents"
-
+# variable string in sed expression, note double quotes
+text=hello
+echo hello world | sed "s/$text/HELLO/"
