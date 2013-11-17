a5bea3c18c21c29932a356bd0a09e26888c967ce
Ming ZHAO <ming.zhao@hotmail.fr>
2013-11-17 01:47:58 +0100

    function

--- python.py
+++ python.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python
 
-# a <> b: true if a != b
-# a **= c : power calculation 3 ** 2 = 9
-# a //= c : 5 // 2 = 2 
+# def function_name( parameters ):
+#    instruments
+#    return [expression]
 
-# x in y: result in "1" if x is a member of sequence y
-# x not in y
+def printname( str ):
+    print str;
+    return;
 
-# x is y
-# x is not y
+printname(" I'm good!");
 
-#
+total=0 # global variable
+
+# variable-length arguments
+def printinfo( arg1, *vartuple ):
+    print "output is:"
+    print arg1
+    for var in vartuple:
+	print var
+    return;
+
+printinfo(10);
+
+sum = lambda arg1, arg2: arg1 + arg2;
+
+print sum(20, 30);
