57be84ac37bbaf7b512838a8cf220869967af2a8
Ming ZHAO <ming.zhao@hotmail.fr>
2013-11-18 10:25:35 +0100

    Python and Bash: Search for files and list results with file permissions

--- python.py
+++ python.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python
 
-# try:
-#    fh = open("testfile", "w")
-#    fh.write("This is my test file for exception handling!!")
-# except IOError:
-#    print "Error: can\'t find file or read data"
-# else:
-#    print "Written content in the file successfully"
-#    fh.close()
-# finally:
-#    this will always be executed
+import stat, sys, os, string, commands
 
-# raise an exception
-# if (condition)
-#	raise "exception", level
+#Getting search pattern from user and assigning it to a list
 
-# try:
-#	instruments
-# except "exception":
-#	instruments
-# else:
-#	other instruments 
+try:
+    #run a 'find' command and assign results to a variable
+    pattern = raw_input("Enter the file pattern to search for:\n")
+    commandString = "find ~/ -path " + pattern
+    commandOutput = commands.getoutput(commandString)
+    findResults = string.split(commandOutput, "\n")
 
-# user-defined exception
-# class Networkerr(RuntimeError):
-#	def __init__(self, arg):
-#	    self.args=arg
-
-# try:
-#    raise Networkerr("bad hostname)"
-# except Networkerr, e:
-# print e.args
+    #output find results, along with permissions
+    print "Files:"
+    print commandOutput
+    print "================================"
+    for file in findResults:
+        mode=stat.S_IMODE(os.lstat(file)[stat.ST_MODE])
+        print "\nPermissions for file ", file, ":"
+        for level in "USR", "GRP", "OTH":
+            for perm in "R", "W", "X":
+                if mode & getattr(stat,"S_I"+perm+level):
+                    print level, " has ", perm, " permission"
+                else:
+                    print level, " does NOT have ", perm, " permission"
+except:
+    print "There was a problem - check the message above"
